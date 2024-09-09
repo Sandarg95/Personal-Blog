babfcee846ea362396be7aae481b52638f9dfc90
Daniel Renaldo GSantana <149911858+Sandarg95@users.noreply.github.com>
2024-09-08 23:50:37 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,14 @@
+// Global object 
+
+// console.log(global);
+
+// setTimeout(() => {
+//     clearInterval(int);
+// }, 3000);
+
+// const int= setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
---
+++ test.js
@@ -0,0 +1,10 @@
+//const rel='Mario';
+
+//console.log(rel);
+
+const greet=(name)=>{
+    console.log(`hello, ${name}`);
+}
+
+greet ('mario');
+greet('yoshi');
