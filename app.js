4b07c54ab9514917d1998462281ea1aa0aeb3978
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-09-22 23:03:35 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+// listen for requests
+app.listen(3000);
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host:', req.hostname);
+//     console.log('path:', req.path);
+//     console.log('method:', req.method);
+//     next();
+// });
+
+app.get('/',(req,res)=>{
+    const blogs= [
+        {title:'Yoshi finds eggs', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title:'Mario finds stars', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title:'How to defeat bowser', snippet:'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs});
+});
+
+app.get('/', (req, res) => {
+  // res.send('<p>home page</p>');
+  res.render('index', {title: 'Home'});
+});
+// app.use((req, res, next) => {
+//     console.log('In the next');
+//     next();
+// });
+
+app.get('/about', (req, res) => {
+  // res.send('<p>about page</p>');
+  res.render('about',{title: 'About'});
+});
+
+// // redirects
+app.get('/create', (req, res) => {
+   res.render('create', {title: 'Create a new Blog'});
+ });
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404',{title: '404'});
+});
